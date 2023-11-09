9877735fe7d364ef2ced51efe7a7a0d2c21335ba
Siddhartha Pati <144094274+1989Sid@users.noreply.github.com>
2023-11-10 03:20:07 +0530

    Add files via upload

---
+++ tk.py
@@ -0,0 +1,54 @@
+import requests
+import os
+
+# Set your GitHub username and access token
+github_username = "1989Sid"
+github_access_token = "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"
+
+# Set the source GitHub account and branch
+source_username = "praveen1994dec"
+branch = "main"
+
+# Set the target GitHub account
+target_username = "1989Sid"
+
+# Function to authenticate with GitHub API
+def get_headers():
+    return {
+        "Authorization": f"Bearer {github_access_token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+# Function to clone repositories from source account to target account
+def clone_repositories():
+    # Get the list of repositories from the source account
+    url = f"https://api.github.com/users/{source_username}/repos"
+    response = requests.get(url, headers=get_headers())
+    repositories = response.json()
+
+    # Clone each repository
+    for repo in repositories:
+        repo_name = repo["name"]
+
+        # Clone the repository
+        os.system(f"git clone --branch {branch} https://github.com/{source_username}/{repo_name}.git")
+
+        # Create a new repository in the target account with the same name
+        create_repo_url = f"https://api.github.com/user/repos"
+        create_repo_payload = {
+            "name": repo_name,
+            "private": False
+        }
+        response = requests.post(create_repo_url, headers=get_headers(), json=create_repo_payload)
+        new_repo = response.json()
+
+        # Push the cloned repository to the new repository
+        os.chdir(repo_name)
+        os.system(f"git remote add origin https://github.com/{target_username}/{repo_name}.git")
+        os.system("git push -u origin main")
+        os.chdir("..")
+
+        print(f"Repository '{repo_name}' cloned and pushed to '{target_username}/{repo_name}'")
+
+if __name__ == "__main__":
+    clone_repositories()
---
+++ xx.py
@@ -0,0 +1,52 @@
+
+import requests
+import os
+
+def get_repos(username, token):
+    url = f'https://api.github.com/users/{username}/repos'
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to get repositories. Status code: {response.status_code}")
+
+def clone_repo(repo_url, repo_name):
+    os.system(f'git clone {repo_url} {repo_name}')
+
+def create_repo(repo_name, token):
+    url = 'https://api.github.com/user/repos'
+    headers = {'Authorization': f'token {token}'}
+    data = {'name': repo_name, 'auto_init': True}
+    response = requests.post(url, headers=headers, json=data)
+    
+    if response.status_code == 201:
+        print(f"Repository '{repo_name}' created successfully.")
+        return response.json()['clone_url']
+    else:
+        raise Exception(f"Failed to create repository. Status code: {response.status_code}")
+
+def push_to_repo(local_repo_path, target_repo_url):
+    os.system(f'cd {local_repo_path} && git remote add target {target_repo_url} && git push target --all')
+
+# GitHub username and access token
+source_username = 'iam-veeramalla'
+target_username = '1989Sid'
+github_access_token = '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'
+
+# Get repositories from source user
+repos = get_repos(source_username, github_access_token)
+
+# Iterate through repositories
+for repo in repos:
+    repo_name = repo['name']
+
+    # Clone the repository
+    clone_repo(repo['clone_url'], repo_name)
+
+    # Create a repository in the target account
+    target_repo_url = create_repo(repo_name, github_access_token)
+
+    # Push the cloned repository to the target repository
+    push_to_repo(repo_name, target_repo_url)
